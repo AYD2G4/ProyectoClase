e11895cc4304a99e8e98d7c4615c289a9e312bc5
WilliamLopez5 <willyslider@gmail.com>
2019-02-16 11:30:11 -0600

    V0.0.2
    Creación de Pruebas TDD

---
+++ ProyectoClase/tests/Feature/ControlVuelosTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ControlVuelosTest extends TestCase
+{
+    /**
+     * Prueba creada para probar si esta retornando correctamente la vista /ControlDeVuelos
+     */
+    public function testControlVuelos(){
+        $response = $this->get('/ControlDeVuelos');
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+    
+    /**
+     * Prueba creada para probar si esta retornando correctamente la vista /EstadoAviones
+     */
+    public function testEstadoAviones(){
+        $response = $this->get('/estadoAviones');
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+    
+    /**
+     * Prueba creada para probar si esta retornando correctamente la vista /definirRuta
+     */
+    public function testDefinirRuta(){
+        $response = $this->get('/definirRuta');
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+}
---
+++ ProyectoClase/tests/Unit/ControlVuelosTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ControlVuelosTest extends TestCase
+{
+    /**
+     * Prueba creada para probar la respueta de la base de datos de la tabla: Aeropuerto
+     * y la columna: Nombre
+     */
+    public function testCrearAeropuertoDB()
+    {
+        $this->assertDatabaseHas('aeropuerto', [
+            'nombre' => 'Aeropuerto Internacional La Aurora'
+        ]);
+    }
+
+    /**
+     * Prueba creada para probar la respueta de la base de datos de la tabla: avion
+     * y la columna: codigo
+     */
+    public function testCrearAvionDB()
+    {
+        $this->assertDatabaseHas('avion', [
+            'codigo' => 'A7575'
+        ]);
+    }
+
+    /**
+     * Prueba creada para probar la respueta de la base de datos de la tabla: vuelo
+     * y la columna: aeropuertosalida
+     */
+    public function testCrearVueloDB()
+    {
+        $this->assertDatabaseHas('vuelo', [
+            'aeropuertosalida' => '1'
+        ]);
+    }
+
+    /**
+    * Prueba para verificar que se puede cargar Menu
+    */
+    public function testMenuControlVuelos(){
+        $response = $this->get('/ControlDeVuelos');
+        $response->assertSeeText('MENU CONTROL DE VUELOS');
+    }
+    
+    /**
+    * Prueba para verificar que se puede ver el Estado de los aviones
+    */
+    public function testEstadoAviones(){
+        $response = $this->get('/estadoAviones');
+        $response->assertSeeText('ESTADO DE AVIONES');
+    }
+    
+    /**
+    * Prueba para verificar que se puede guardar nuevo registro de vuelo
+    */
+    public function testRegistroVuelo(){
+        $response = $this->get('/definirRuta');
+        $response->assertSeeText('DEFINIR RUTA');
+    }
+    
+
+}
