d35726eea5aeb0b6e42480400f2a35f6b0ac95df
deahtom123 <201504200@ingenieria.usac.edu.gt>
2019-02-25 16:54:26 -0600

    V0.2.0

--- ProyectoClase/resources/views/PagoTarjeta/PagoTarjeta.blade.php
+++ ProyectoClase/resources/views/PagoTarjeta/PagoTarjeta.blade.php
@@ -2,22 +2,13 @@
 @section('content')
 <div class="container-fluid">
 <a href="/ControlDeVuelos"><button class="btn-warning btn-raised btn-sm"><i class="zmdi zmdi-graduation-cap zmdi-hc-fw"></i>Menu Control de Vuelos</button></a><br>
-	<center>	<p class="lead">  ESTADO DE AVIONES </p></center>
-	<div class="col-xs-12">
-      <div class="table-responsive">
-        <table class="table table-hover text-center">
-          <thead>
-			<tr>
-			<th style="width: 5%"> ID </th>
-			<th style="width: 5%"> Codigo </th>
-			<th style="width: 20%"> Marca </th>
-			<th style="width: 5%"> Capacidad </th>
-			<th style="width: 10%"> Estado </th>
-			</tr>
-          </thead>
-		  <tbody>
-		  </tbody>
-			
+	<center>	<p class="lead">  Pago Con Tarjeta </p></center>
+	<div class="container-fluid">
+	<form  method="post">
+	@csrf
+	<input id="Nombre" type="text" name="Nombre">
+	</form>
+	</div>		
 		
 				
 				
