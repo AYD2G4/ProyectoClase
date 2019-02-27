81d182f7ea17ca3cc23912abf516916b8ea59362
WilliamLopez5 <willyslider@gmail.com>
2019-02-26 23:05:41 -0600

    V1.1.0
    Creación de Pruebas TDD

---
+++ ProyectoClase/tests/Feature/ManejoHorariosTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ManejoHorariosTest extends TestCase
+{
+    /**
+     * Prueba creada para probar si esta retornando correctamente la vista /ManjeoHorario
+     */
+    public function testManejoHorario(){
+        $response = $this->get('/ManejoHorario');
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+    
+    /**
+    * Prueba para verificar que se puede cargar ManejoHorario
+    */
+    public function testMenuControlVuelos(){
+        $response = $this->get('/ControlDeVuelos');
+        $response->assertSeeText('MANEJO DE HORARIOS');
+    }
+    
+    
+
+}
