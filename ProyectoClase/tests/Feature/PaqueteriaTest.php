3070a89cc93465f31c175393e72fdb6c7017a3fe
miguelruano3 <miguelruano35@gmail.com>
2019-03-13 12:26:51 -0600

    V.0.0.2
    Probando jenkins

--- ProyectoClase/tests/Feature/PaqueteriaTest.php
+++ ProyectoClase/tests/Feature/PaqueteriaTest.php
@@ -28,7 +28,7 @@ class PaqueteriaTest extends TestCase
     {
         $controller = new PaqueteriaController();
         $paquete=$controller->MetodoCrearPaquete(2,23,"Lacteos",23,12,23, "En espera");
-        $this->assertNotNull($paquete);
+        $this->assertNull($paquete);
         $paquete->delete();
     }
     /**
