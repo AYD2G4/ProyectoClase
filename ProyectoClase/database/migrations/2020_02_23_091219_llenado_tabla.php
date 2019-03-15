e9a6e6472be75b548f2827a80076146e0fb2fa9f
Miguel Ruano <miguelruano35@gmail.com>
2019-03-15 03:11:36 -0600

    arreglo del llenado temporal

--- ProyectoClase/database/migrations/2020_02_23_091219_llenado_tabla.php
+++ ProyectoClase/database/migrations/2020_02_23_091219_llenado_tabla.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class Llenado extends Migration
+class LlenadoTabla extends Migration
 {
     /**
      * Run the migrations.
